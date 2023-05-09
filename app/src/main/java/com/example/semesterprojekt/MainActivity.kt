ca7b7e6f2c767cad9d6585ce2f7fa5306893caef
Johannes-DP <92081795+Johannes-DP@users.noreply.github.com>
2023-05-09 10:16:43 +0200

    Basic Navigation

--- app/src/main/java/com/example/semesterprojekt/MainActivity.kt
+++ app/src/main/java/com/example/semesterprojekt/MainActivity.kt
@@ -3,13 +3,10 @@ package com.example.semesterprojekt
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.semesterprojekt.navigation.Navigation
 import com.example.semesterprojekt.ui.theme.SemesterprojektTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,12 +15,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             SemesterprojektTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
+                Navigation()
             }
         }
     }
